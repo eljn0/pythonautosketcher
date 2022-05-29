61686ebb53c1c59adadcf0f2d20030af4c125996
Eljan Guluyev <77200703+eljn0@users.noreply.github.com>
2022-05-29 22:56:14 +0400

    Create sktch.py

---
+++ sktch.py
@@ -0,0 +1,9 @@
+import cv2
+image = cv2.imread("image.jpg")
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+inverted = 255-gray_image
+blur = cv2.GaussianBlur(inverted, (21,21), 0)
+invertedblur = 255-blur
+sketch = cv2.divide(gray_image, inverted, scale=256.0)
+cv2.imwrite("sketch_image.png", sketch)
+cv2.imshow("image, sketch")
